9916d3132956f80091ca74458afefcbf9fca2aea
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-24 17:45:29 +0900

    #1 add: objectの型指定

--- basic-typescript/app.js
+++ basic-typescript/app.js
@@ -1,13 +1,5 @@
-function add(n1, n2, showReuslt, resultPhrase) {
-    if (showReuslt) {
-        console.log("".concat(resultPhrase).concat(n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Resule: ';
-var result = add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'neko',
+    age: 20
+};
+console.log(person);
--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,14 +1,6 @@
-function add(n1: number, n2: number, showReuslt: boolean, resultPhrase: string) {
- if (showReuslt){
-    console.log(`${resultPhrase}${n1 + n2}`);
- }else {
-     return n1 + n2;
- }
+const person = {
+    name: 'neko',
+    age: 20
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Resule: ';
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person);
