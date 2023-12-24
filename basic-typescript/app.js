fe8a27cc743e3300a72379dceba9d5e1d89c1934
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-24 17:49:46 +0900

    #1 add: array型 の使い方

--- basic-typescript/app.js
+++ basic-typescript/app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'neko',
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'Cooking'],
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,6 +1,14 @@
 const person = {
     name: 'neko',
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'Cooking'],
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person);
+
+for (const hobby of person.hobbies){
+    console.log(hobby);
+}
