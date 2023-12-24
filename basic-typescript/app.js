55c386d0e0aff90071bcb012ec6019f2821b8170
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-24 17:09:20 +0900

    #1 add: 型指定と型推論について学ぶ

--- basic-typescript/app.js
+++ basic-typescript/app.js
@@ -1,7 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showReuslt, resultPhrase) {
+    if (showReuslt) {
+        console.log("".concat(resultPhrase).concat(n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Resule: ';
+var result = add(number1, number2, printResult, resultPhrase);
--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,9 +1,14 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showReuslt: boolean, resultPhrase: string) {
+ if (showReuslt){
+    console.log(`${resultPhrase}${n1 + n2}`);
+ }else {
+     return n1 + n2;
+ }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Resule: ';
 
-const result = add(number1, number2);
-console.log(result)
+const result = add(number1, number2, printResult, resultPhrase);
