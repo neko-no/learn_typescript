024d4de11c2c65fa2e3548d36102c9e0a890cfba
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-29 15:25:20 +0900

    #1 add: Union型の実装の追加

--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,25 +1,15 @@
-enum Role {
-    ADMIN,
-    READ_ONLY_USER,
-    AUTHOR
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-const person = {
-    name: 'neko',
-    age: 20,
-    hobbies: ['sports', 'Cooking'],
-    role: Role.ADMIN
-}
+const combineAges = combine(30, 26);
+console.log(combineAges);
 
-let favoriteActivities: any[];
-favoriteActivities = ['Sports'];
-
-console.log(person);
-
-for (const hobby of person.hobbies){
-    console.log(hobby);
-}
-
-if (person.role === Role.ADMIN){
-    console.log("表示可能な値になります");
-}
+const combineNames = combine('Mike', 'Anna');
+console.log(combineNames);
