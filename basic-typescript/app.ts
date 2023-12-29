02dda43c77173db76f3bdbfd6ae471839d40404b
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-29 19:16:35 +0900

    #1 add: エイリアス型の定義を学ぶ

--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-text' | 'as-number';
+
 function combine(
-    input1: number | string,
-    input2: number | string,
-    resultConversion: 'as-text' | 'as-number'
+    input1: Combinable,
+    input2: Combinable,
+    resultConversion: ConversionDescriptor
     ) {
     let result;
     if ((typeof input1 === 'number' && typeof input2 === 'number') ||
