1115730c01da5475b75e10561943e0e5649d0629
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-29 15:37:54 +0900

    #1 add: Union と Literal型を学ぶ

--- basic-typescript/app.js
+++ basic-typescript/app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,9 +6,14 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultConversion === 'as-number') {
+        return +result;
+    }
+    return result.toString();
 }
-var combineAges = combine(30, 26);
+var combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
-var combineNames = combine('Mike', 'Anna');
+var combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+var combineNames = combine('Mike', 'Anna', 'as-text');
 console.log(combineNames);
--- basic-typescript/app.ts
+++ basic-typescript/app.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-text' | 'as-number'
+    ) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number'){
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+
+    if (resultConversion === 'as-number'){
+        return +result;
+    }
+    return result.toString();
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Mike', 'Anna');
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Mike', 'Anna', 'as-text');
 console.log(combineNames);
