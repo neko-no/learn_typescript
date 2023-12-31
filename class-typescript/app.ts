e09ec6e8e90eb041641e24ecef9c10d0cbb60f0b
neko-no <101441574+neko-no@users.noreply.github.com>
2023-12-31 21:51:57 +0900

    #7 add: private変数の定義と，セッターを学ぶ

--- class-typescript/app.ts
+++ class-typescript/app.ts
@@ -1,5 +1,6 @@
 class Department {
      name: string;
+     private employees: string[] = [];
 
      constructor (n: string) {
           this.name = n;
@@ -8,8 +9,19 @@ class Department {
      describe(this: Department) {
           console.log(this.name);
      }
+
+     addEmployee(employee: string) {
+          this.employees.push(employee);
+     }
+
+     printEmployeeInformation(){
+          console.log(this.employees.length);
+          console.log(this.employees);
+     }
 }
 
 const accounting =  new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.describe();
 console.log(accounting);
--- class-typescript/dist/app.js
+++ class-typescript/dist/app.js
@@ -1,12 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log(this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.describe();
 console.log(accounting);
