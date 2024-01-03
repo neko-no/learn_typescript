9c318b651e167fabfc01e7a02e98973eff61b8d0
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-03 09:34:07 +0900

    #9 add: 型ガードを学ぶ（基本型・オブジェクト・クラスで指定が異なることがわかった）

--- advanced-type-typescript/app.ts
+++ advanced-type-typescript/app.ts
@@ -15,3 +15,60 @@ const e1: ElevateEmployee = {
      privileges: ['create-server'],
      startDate: new Date()
 }
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Univarsal = Combinable & Numeric;
+
+function add (a: Combinable, b:Combinable) {
+     if(typeof a === 'string' || typeof b === 'string') {
+          return a.toString() + b.toString();
+     }
+
+     return a + b;
+}
+
+type UnknownEmoployee = Admin | Employee;
+
+function printInfoEmployee(emp: UnknownEmoployee){
+      console.log(emp.name);
+      if ('privileges' in emp) {
+           console.log(`Privilages: ${emp.privileges}`);
+      }
+      if('startDate' in emp) {
+          console.log(`startDate: ${emp.startDate}`);
+      }
+}
+
+printInfoEmployee(e1);
+
+
+class Car {
+     drive () {
+          console.log('運転中...')
+     }
+}
+
+class Truck {
+     drive () {
+          console.log('トラックを運転中...')
+     }
+
+     loadCargo (amount: number){
+          console.log(`荷物を乗せています　${amount}`)
+     }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+     vehicle.drive();
+
+     if(vehicle instanceof Truck){
+          vehicle.loadCargo(1000);
+     }
+}
--- advanced-type-typescript/dist/app.js
+++ advanced-type-typescript/dist/app.js
@@ -4,3 +4,40 @@ const e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printInfoEmployee(emp) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+        console.log(`Privilages: ${emp.privileges}`);
+    }
+    if ('startDate' in emp) {
+        console.log(`startDate: ${emp.startDate}`);
+    }
+}
+printInfoEmployee(e1);
+class Car {
+    drive() {
+        console.log('運転中...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('トラックを運転中...');
+    }
+    loadCargo(amount) {
+        console.log(`荷物を乗せています　${amount}`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
