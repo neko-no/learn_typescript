808b30e9bd908d8c188ba9b7b8f0cb29e6562795
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-03 09:13:38 +0900

    #9 add: 交差型について学ぶ

--- advanced-type-typescript/app.ts
+++ advanced-type-typescript/app.ts
@@ -1,32 +1,17 @@
-interface Named {
+type Admin = {
      name: string;
-     age: number;
+     privileges: string[];
 }
 
-interface Greetable extends Named{
-    greet(pharase: string): void;
+type Employee = {
+     name: string;
+     startDate: Date;
 }
 
+type ElevateEmployee = Admin & Employee;
 
-class Person implements Greetable {
-     constructor(public name: string, public age: number) {}
-
-     greet(pharase: string): void {
-          console.log(`${this.name} ${pharase}`)
-     }
-}
-
-let user1:Greetable;
-user1 = new Person('Max', 30);
-
-user1.greet('hello')
-
-interface Addfn {
-     (a: number, b: number): number;
-}
-
-let add: Addfn;
-
-add = (n1, n2) => {
-     return n1 + n2;
+const e1: ElevateEmployee = {
+     name: 'Max',
+     privileges: ['create-server'],
+     startDate: new Date()
 }
