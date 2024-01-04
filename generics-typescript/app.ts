ff792969210c0acb7abf1c8e5996c5d42f231a73
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-04 10:22:54 +0900

    #10 add: 自分で定義した型を用いてジェネリクスを定義する方法を学ぶ

--- generics-typescript/app.ts
+++ generics-typescript/app.ts
@@ -11,3 +11,18 @@ function merge<T extends {}, U>(obj1: T, obj2: U) {
 }
 
 console.log(merge({name: 'Max'}, {age: 30}));
+
+interface Lengthy {
+     length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+     let description = '値が格納されていません'
+     if (element.length > 0) {
+          description = `値は${element.length}個です．`
+     }
+
+     return [element, description]
+}
+
+console.log(countAndDescribe('お疲れ様です．これからも頑張ってください'))
--- generics-typescript/dist/app.js
+++ generics-typescript/dist/app.js
@@ -9,3 +9,11 @@ function merge(obj1, obj2) {
     return Object.assign(obj1, obj2);
 }
 console.log(merge({ name: 'Max' }, { age: 30 }));
+function countAndDescribe(element) {
+    let description = '値が格納されていません';
+    if (element.length > 0) {
+        description = `値は${element.length}個です．`;
+    }
+    return [element, description];
+}
+console.log(countAndDescribe('お疲れ様です．これからも頑張ってください'));
