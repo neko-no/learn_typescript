8b6d67e733e3dda75fd09b4d943becd726f96e48
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-04 09:48:59 +0900

    #10 add: ジェネリクスに任意の型を受け取る方法を学ぶ

--- generics-typescript/app.ts
+++ generics-typescript/app.ts
@@ -5,3 +5,9 @@ const promise = new Promise<string>((res ,rej)=>{
           res('終わりました!!')
      },2000)
 })
+
+function merge<T extends {}, U>(obj1: T, obj2: U) {
+     return Object.assign(obj1,obj2);
+}
+
+console.log(merge({name: 'Max'}, {age: 30}));
--- generics-typescript/dist/app.js
+++ generics-typescript/dist/app.js
@@ -1,72 +1,11 @@
 "use strict";
-var _a;
-const e1 = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date()
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+const names = ['Max', 'Yuto'];
+const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+        res('終わりました!!');
+    }, 2000);
+});
+function merge(obj1, obj2) {
+    return Object.assign(obj1, obj2);
 }
-function printInfoEmployee(emp) {
-    console.log(emp.name);
-    if ('privileges' in emp) {
-        console.log(`Privilages: ${emp.privileges}`);
-    }
-    if ('startDate' in emp) {
-        console.log(`startDate: ${emp.startDate}`);
-    }
-}
-printInfoEmployee(e1);
-class Car {
-    drive() {
-        console.log('運転中...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('トラックを運転中...');
-    }
-    loadCargo(amount) {
-        console.log(`荷物を乗せています　${amount}`);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log(`移動速度: ${speed}`);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 30 });
-// const paragrah = <HTMLInputElement>document.getElementById('message-output');
-const paragrah = document.getElementById('message-output');
-paragrah.innerHTML = 'input!!!!! ';
-const errorBag = {
-    email: '正しいメールアドレスではありません．'
-};
-const fetchData = {
-    id: 'u1',
-    name: 'user1',
-    job: {
-        title: 'Developer',
-        description: 'TypeScript',
-    }
-};
-console.log((_a = fetchData.job) === null || _a === void 0 ? void 0 : _a.title);
+console.log(merge({ name: 'Max' }, { age: 30 }));
