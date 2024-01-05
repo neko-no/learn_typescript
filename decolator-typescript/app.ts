74c989894d902644ab43274405e6efca95e46290
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-05 18:03:49 +0900

    #13 add: デコレータを呼び出す位置による定義の違いを学ぶ

--- decolator-typescript/app.ts
+++ decolator-typescript/app.ts
@@ -27,3 +27,53 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+function Log(target: any, prototypeName: string | Symbol){
+     console.log('Prototype デコレータ');
+     console.log(target, prototypeName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+     console.log('Accessorデコレータ');
+     console.log(target);
+     console.log(name);
+     console.log(descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor){
+     console.log('Methodデコレータ');
+     console.log(target);
+     console.log(name);
+     console.log(descriptor);
+}
+
+function Log4(target: any, name: string, position: number){
+     console.log('Prametor デコレータ');
+     console.log(target);
+     console.log(name);
+     console.log(position);
+}
+class Product {
+     @Log
+     title: string;
+     private _price: number;
+
+     constructor(t: string, n: number){
+          this.title = t;
+          this._price = n;
+     }
+
+     @Log2
+     set price(val: number){
+          if (val > 0){
+               this._price = val;
+          } else {
+               throw new Error('不正な価格です．0以上の値を設定してください')
+          }
+     }
+
+     @Log3
+     getPriceWithTax(@Log4 tax: number) {
+          return this._price * (1 + tax);
+     }
+}
--- decolator-typescript/dist/app.js
+++ decolator-typescript/dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
     return function (constructor) {
         console.log(logString);
@@ -29,3 +32,52 @@ Person = __decorate([
 ], Person);
 const pers = new Person();
 console.log(pers);
+function Log(target, prototypeName) {
+    console.log('Prototype デコレータ');
+    console.log(target, prototypeName);
+}
+function Log2(target, name, descriptor) {
+    console.log('Accessorデコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Methodデコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Prametor デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, n) {
+        this.title = t;
+        this._price = n;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('不正な価格です．0以上の値を設定してください');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
