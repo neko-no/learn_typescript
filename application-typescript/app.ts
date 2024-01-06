75b1374f3988b04f8ad8ffb9ed9dcc2e2f8b73fd
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-06 12:20:32 +0900

    #15 add: イベントリスナーを追加

--- application-typescript/app.ts
+++ application-typescript/app.ts
@@ -2,15 +2,38 @@ class ProjectInput {
      templateElement: HTMLTemplateElement;
      hostElement: HTMLDivElement;
      element: HTMLFormElement;
+     titleInputElement: HTMLInputElement;
+     descriptionInputElement: HTMLInputElement;
+     mandayInputElement: HTMLInputElement;
 
      constructor(){
+          // フォームの表示を行う
           this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
           this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
           const importedNode = document.importNode(this.templateElement.content, true);
 
           this.element = importedNode.firstElementChild as HTMLFormElement;
+          this.element.id = 'user-input';
+
+          this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+          this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+          this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
+
+          this.configure();
           this.attach();
+     }
+
+     private submitHundler(event: Event) {
+          event.preventDefault();
+          console.log(this.titleInputElement.value);
+          // console.log(this.descriptionInputElement.value);
+          // console.log(this.mandayInputElement.value);
+
+     }
+
+     private configure(){
+          this.element.addEventListener('submit', this.submitHundler.bind(this))
      }
 
      private attach(){
--- application-typescript/dist/app.js
+++ application-typescript/dist/app.js
@@ -1,11 +1,26 @@
 "use strict";
 class ProjectInput {
     constructor() {
+        // フォームの表示を行う
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.mandayInputElement = this.element.querySelector('#manday');
+        this.configure();
         this.attach();
+    }
+    submitHundler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+        // console.log(this.descriptionInputElement.value);
+        // console.log(this.mandayInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHundler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
