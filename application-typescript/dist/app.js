70b17f6028028793087349643fef56f35e0d3294
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-07 21:53:09 +0900

    #15 add: 処理の共通化を行う抽象メソッドの実装

--- application-typescript/dist/app.js
+++ application-typescript/dist/app.js
@@ -74,6 +74,19 @@ function autobind(target, methodName, desriptor) {
     };
     return adjDescriptor;
 }
+class Component {
+    constructor(templateId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        newElementId && (this.element.id = newElementId);
+        this.attach(insertAtStart);
+    }
+    attach(insertAtStart) {
+        this.hostElement.insertAdjacentElement(insertAtStart ? 'afterbegin' : 'beforeend', this.element);
+    }
+}
 class ProjectList {
     constructor(type) {
         this.type = type;
