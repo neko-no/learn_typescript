4012535fd20ac4b30f8545fedec04af5e31eeb64
neko-no <101441574+neko-no@users.noreply.github.com>
2024-01-20 14:09:08 +0900

    #25 削除機能を実装

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -6,16 +6,23 @@ import { Todo } from './todo.model';
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([{id: 't1', text: 'TS講座の完了'}])
+
   const todoAddHundler = (text: string) => {
     setTodos(prevTodos => [
       ...prevTodos,
       {id: Math.random().toString(), text: text}]
     )
   }
+
+  const todoDeleteHundler = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHundler} />
-      <TodoList items={todos}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHundler}/>
     </div>
   );
 }
--- react-typescript/src/components/TodoList.tsx
+++ react-typescript/src/components/TodoList.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 interface TodoListProps {
-    items: {id: string, text: string}[]
+    items: {id: string, text: string}[],
+    onDeleteTodo: (id:string) => void
 }
 
-const TodoList: React.FC<TodoListProps> = ({items}) => {
+const TodoList: React.FC<TodoListProps> = ({items, onDeleteTodo}) => {
     return (
         <ul>
-            {items.map(todo => <li key={todo.id}>{todo.text}</li>)}
+            {items.map(todo =>
+            <li key={todo.id}>
+                <span>{todo.text}</span>
+                {/* <button onClick={() => onDeleteTodo(todo.id)}>削除</button> */}
+                <button onClick={onDeleteTodo.bind(null, todo.id)}>削除</button>
+            </li>)}
         </ul>
     )
 }
